e14ee96a085f6ae7f71643afd62a20e7d432cc5d
Amit Ferman <amitferman@gmail.com>
2021-07-07 15:29:56 -0700

    Add Navbar, Map, Preview

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const previewTextEl = document.getElementById("preview-text");
+
+var map = L.map('map').setView([47.6532, -122.3074], 16);
+
+L.tileLayer( 'https://api.mapbox.com/styles/v1/aferman/ckhvetwgy0bds19nznkfvodbx/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWZlcm1hbiIsImEiOiJja2ZrZXJvbjUwZW5wMnhxcjdyMXc3ZjRnIn0.WGdId2uO9XokPaJmaxlLXg', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: ['a','b','c']
+}).addTo( map );
+
+fetch('Nodes.txt')
+  .then(response => response.text())
+  .then(text => updatePreview(text))
+
+function updatePreview(text) {
+    previewTextEl.textContent = text; 
+}
+
+/*
 // Returns the downloaded file given a containing url and file name.
 // download("https://huskynavigationcontent.azurewebsites.net/Nodes.txt","Nodes.txt")
 function download(url, filename) {
@@ -16,3 +34,4 @@ function download(url, filename) {
 function nodesSetup(nodes.txt) {
 
 }
+*/
