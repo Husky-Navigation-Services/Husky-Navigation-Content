fc7c22f35e96fe225308b3e3218a5390027eee5d
Daniel Berezansky <23129330+dberezansky@users.noreply.github.com>
2021-07-07 12:01:37 -0700

    Basic UI Initial Setup

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+// Returns the downloaded file given a containing url and file name.
+// download("https://huskynavigationcontent.azurewebsites.net/Nodes.txt","Nodes.txt")
+function download(url, filename) {
+    fetch(url).then(function(t) {
+        return t.blob().then((b)=>{
+            var a = document.createElement("a");
+            a.href = URL.createObjectURL(b);
+            a.setAttribute("download", filename);
+            a.click();
+        }
+        );
+    });
+}
 
+// Labels nodes and paths given the Nodes.txt file.
+function nodesSetup(nodes.txt) {
+
+}
